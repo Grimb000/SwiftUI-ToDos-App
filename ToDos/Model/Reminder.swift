8af337b6f1c82e9dd0b037606993664a2e29661e
Grimb000 <valorantik0005@gmail.com>
2025-06-25 01:27:09 +0300

    ToDos App

---
+++ ToDos/Model/Reminder.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Reminder {
+    var name: String
+    var isCompleted = false
+    
+    init(name: String, isCompleted: Bool = false) {
+        self.name = name
+        self.isCompleted = isCompleted
+    }
+}
---
+++ ToDos/ToDosApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+        .modelContainer(for: ReminderList.self)
+    }
+}
